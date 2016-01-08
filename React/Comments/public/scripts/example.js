fb3676ce1425f07d7f244f65cac52d758f786140
GabeHarms <gabeharms@cacpdev-gabehar.local>
2016-01-08 13:42:12 -0800

    Adding Comment list and form skeletons

--- React/Comments/public/scripts/example.js
+++ React/Comments/public/scripts/example.js
@@ -1,14 +1,39 @@
 
-// Code for building the comment box
+// Comment Box class
 var CommentBox = React.createClass({
   render: function() { // Only function in the 'CommentBox' class
     return (
       <div className="commentBox">
-        Hello, world! I am a CommentBox.
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
       </div>
     );
   }
 });
+
+// Comment list Class
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+});
+
+// Comment Form Class
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
 
 // Instantiates the root component, starts the framework, 
 // and injects the markup into a raw DOM element, provided as the second argument.
