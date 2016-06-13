f3368fd48f13f902004435d670a1287bc59e286d
Gabe Harms <gabeharms@gmail.com>
2016-06-13 11:04:24 -0700

    Fixing Bug

--- Iron-On-Rust/httpservice/src/client.rs
+++ Iron-On-Rust/httpservice/src/client.rs
@@ -5,7 +5,7 @@ use std::io::Read;
 
 fn main() {
     let client = Client::new();
-    let res = client.post("http://localhost:3000/set").body("Just trust the Rust").send().unwrap();
+    let res = client.post("http://localhost:3000/set").body(r#"{ "msg": "Just trust the Rust" }"#).send().unwrap();
     assert_eq!(res.status, hyper::Ok);
     let mut res = client.get("http://localhost:3000/").send().unwrap();
     assert_eq!(res.status, hyper::Ok);
