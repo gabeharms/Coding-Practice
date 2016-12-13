a3ff34a11fb7b9ece6b1d71b2447b1e5c63f0430
Gabe Harms <gabeharms@gmail.com>
2016-12-12 16:03:36 -0800

    Implementing MergeSort and QuickSort

--- Java/algorithms/src/main/java/com/gabeharms/sorting/MergeSort.java
+++ Java/algorithms/src/main/java/com/gabeharms/sorting/MergeSort.java
@@ -1,6 +1,7 @@
 package com.gabeharms.sorting;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class MergeSort<T extends Comparable<T>> extends SortingAlgorithm<T>
 {
@@ -10,8 +11,93 @@ public class MergeSort<T extends Comparable<T>> extends SortingAlgorithm<T>
   }
 
   public List<T> sort() {
-
+    list = mergeSort(list);
     return this.list;
+  }
+
+  private List<T> mergeSort(List<T> sortedList)
+  {
+    if (sortedList.size() < 2)
+    {
+      return sortedList;
+    }
+    else
+    {
+      int splitIndex = findSplitIndex(sortedList);
+      List<T> subsetA = getSubset(sortedList, 0, splitIndex);
+      List<T> subsetB = getSubset(sortedList, splitIndex, sortedList.size());
+      return combine(mergeSort(subsetA), mergeSort(subsetB));
+    }
+  }
+
+  private int findSplitIndex(List<T> list)
+  {
+    return list.size() / 2;
+  }
+
+  private List<T> getSubset(List<T> list, int startIndex, int endIndex)
+  {
+    return new ArrayList<T>(list.subList(startIndex, endIndex));
+  }
+
+  private List<T> combine(List<T> subsetA, List<T> subsetB)
+  {
+    List<T> mergedSet = new ArrayList<T>();
+
+    while (isListNotEmpty(subsetA) && isListNotEmpty(subsetB))
+    {
+      pluckLeastFrom(mergedSet, subsetA, subsetB);
+    }
+
+    while (isListNotEmpty(subsetA))
+    {
+      pluckFirstFromTo(subsetA, mergedSet);
+    }
+
+    while (isListNotEmpty(subsetB))
+    {
+      pluckFirstFromTo(subsetB, mergedSet);
+    }
+
+    return mergedSet;
+  }
+
+  private boolean isListNotEmpty(List<T> list)
+  {
+    return list.size() != 0;
+  }
+
+  private void pluckLeastFrom(List<T> destination, List<T> subsetA, List<T> subsetB)
+  {
+    if (hasLeastFirstValueThan(subsetA, subsetB))
+    {
+      pluckFirstFromTo(subsetA, destination);
+    }
+    else
+    {
+      pluckFirstFromTo(subsetB, destination);
+    }
+  }
+
+  private boolean hasLeastFirstValueThan(List<T> subsetA, List<T> subsetB)
+  {
+    return getFirst(subsetA).compareTo(getFirst(subsetB)) <= 0;
+  }
+
+  private T getFirst(List<T> list)
+  {
+    return list.get(0);
+  }
+
+  private T removeFirst(List<T> list)
+  {
+    return list.remove(0);
+  }
+
+  private void pluckFirstFromTo(List<T> from, List<T> to)
+  {
+    T value = removeFirst(from);
+    to.add(value);
   }
 
   public static void main( String[] args )
--- Java/algorithms/src/main/java/com/gabeharms/sorting/QuickSort.java
+++ Java/algorithms/src/main/java/com/gabeharms/sorting/QuickSort.java
@@ -11,8 +11,59 @@ public class QuickSort<T extends Comparable<T>> extends SortingAlgorithm<T>
   }
 
   public List<T> sort() {
+    quickSort(0, list.size()-1);
 
     return this.list;
+  }
+
+  private void quickSort(int start, int end)
+  {
+    if (end-start > 2)
+    {
+      int partition = partitionArray(start, end);
+      quickSort(0, partition-1);
+      quickSort(partition+1, end);
+    }
+  }
+
+  private int partitionArray(int start, int end)
+  {
+    int swaps = 0;
+    T partitionValue = list.get(end);
+    for (int i = start; i <= end; i++)
+    {
+      if (isGreaterThan(list.get(i), partitionValue))
+      {
+        moveValueFromTo(i, getPartitionIndex(end, swaps)-1);
+        swaps++;
+        i--;
+      }
+    }
+
+    moveValueFromTo(getPartitionIndex(end, swaps), end); // Moves partitionValue to the partitionIndex
+    return end-swaps;
+  }
+
+  private boolean isGreaterThan(T aValue, T bValue)
+  {
+    return aValue.compareTo(bValue) > 0;
+  }
+
+  private void swap(int a, int b) {
+    T aValue = list.get(a);
+    list.set(a, list.get(b));
+    list.set(b, aValue);
+  }
+
+  private int getPartitionIndex(int end, int swaps)
+  {
+    return end-swaps;
+  }
+
+  private void moveValueFromTo(int oldIndex, int newIndex)
+  {
+    T partitionValue = list.remove(oldIndex);
+    list.add(newIndex, partitionValue);
   }
 
   public static void main( String[] args )
