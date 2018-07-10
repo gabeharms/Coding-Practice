dd1225aac04e9a8490a4a9a8b06073d8e984cd23
Gabe Harms <gabeharms@gmail.com>
2018-07-10 11:25:17 -0700

    Updating routes & handling persistance / validation of request

--- Clojure/guestbook/src/clj/guestbook/routes/home.clj
+++ Clojure/guestbook/src/clj/guestbook/routes/home.clj
@@ -1,18 +1,44 @@
 (ns guestbook.routes.home
   (:require [guestbook.layout :as layout]
             [guestbook.db.core :as db]
-            [compojure.core :refer [defroutes GET]]
+            [compojure.core :refer [defroutes GET POST]]
             [ring.util.http-response :as response]
+            [struc.core :as st]
             [clojure.java.io :as io]))
+
+(def message-schema
+  [[:name
+    st/required
+    st/string]
+   [:message
+    st/required
+    st/string
+    {:message "message must contain at least 10 characters"}
+    {:validate #(> (count %) 9)}]])
+
+defn validate-message [params]
+  (first (st/validate params message-schema))
+
+(defn save-message! [{:keys [params]}]
+  (if-let [errors (validate-message params)]
+    (-> (response/found "/")
+        (assoc :flash (assoc params :errors errors)))
+    (do
+      (db/save-message!
+        (assoc params :timestamp (java.util.Date.)))
+      (response/found "/"))))
 
 (defn home-page []
   (layout/render
-    "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+    "home.html" 
+    (merge {:messages (db/get-messagess)}
+           (select-keys flash [:name :message :errors]))))
 
 (defn about-page []
   (layout/render "about.html"))
 
 (defroutes home-routes
-  (GET "/" [] (home-page))
+  (GET "/" request (home-page request))
+  (POST "/" request (save-message! request))
   (GET "/about" [] (about-page)))
 
