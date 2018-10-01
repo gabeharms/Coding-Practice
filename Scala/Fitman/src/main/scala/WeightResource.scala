f80b590daf774cf344f3a72cf68fb0bc124d8d84
Gabe Harms <gabeharms@gmail.com>
2018-10-01 13:33:28 -0700

    Adding logging

--- Scala/Fitman/src/main/scala/WeightResource.scala
+++ Scala/Fitman/src/main/scala/WeightResource.scala
@@ -1,21 +1,26 @@
 import com.twitter.finatra.http.Controller
 import com.twitter.finagle.http.Request
+import com.twitter.inject.Logging
 
  
 import scala.collection.mutable
-class WeightResource extends Controller {
+class WeightResource extends Controller with Logging {
     val db = mutable.Map[String, List[Weight]]()
 
     get("/weights/:user") { request: Request =>
+        info( s"""finding weight for user ${request.params("user")}""")
         db.getOrElse(request.params("user"), List())
     }
 
     post("/weights") { weight: Weight => 
-        val weightsForUser = db.get(weight.user) match {
-            case Some(weights) => weights :+ weight
-            case None => List(weight)
+        val r = time(s"Total time take to post weight for user '${weight.user}' is %d ms") {
+            val weightsForUser = db.get(weight.user) match {
+                case Some(weights) => weights :+ weight
+                case None => List(weight)
+            }
+            db.put(weight.user, weightsForUser)
+            response.created.location(s"/weights/${weight.user}")
         }
-        db.put(weight.user, weightsForUser)
-        response.created.location(s"/weights/${weight.user}")
+        r
     }
 }
