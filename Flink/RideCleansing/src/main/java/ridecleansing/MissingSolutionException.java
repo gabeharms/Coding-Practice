e7226d959c72fc92e25c66ba9684daf89af9fbba
Gabe Harms <gabe.harms@procore.com>
2021-10-19 12:02:36 -0400

    Adding flink exercises

---
+++ Flink/RideCleansing/src/main/java/ridecleansing/MissingSolutionException.java
@@ -0,0 +1,11 @@
+package ridecleansing;
+
+/**
+ * Exception denoting a missing solution (results in tests verifying the solution instead).
+ */
+public class MissingSolutionException extends Exception {
+	/**
+	 * Create new exception.
+	 */
+	public MissingSolutionException() {}
+}
